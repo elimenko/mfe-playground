7a7d8c379c77094deebedfbf4074a33dd22b1dfa
Egor Limenko <egor.limenko@gmail.com>
2023-08-22 10:15:23 +0200

    Test storage partitioning

--- root-react-app/src/App.tsx
+++ root-react-app/src/App.tsx
@@ -6,6 +6,13 @@ import MFRemoteApp from './MFRemoteApp';
 function App() {
   const [count, setCount] = useState(0)
 
+
+  const onLSEvent = (e: StorageEvent) => {
+    console.log(`Received event: ${e.key} ${e.oldValue} -> ${e.newValue}`);
+  }
+
+  window.addEventListener('storage', onLSEvent);
+
   return (
     <>
       <h1>Root app</h1>
@@ -17,7 +24,8 @@ function App() {
       <div>
         <h2>Microfrontends</h2>
         <div className="mfe-grid">
-          <iframe src="http://myapp.localhost" title="microfrontend1"></iframe>
+          <iframe src="http://myapp1.localhost" title="microfrontend1"></iframe>
+          <iframe src="http://myapp2.localhost" title="microfrontend2"></iframe>
           <MFRemoteApp rootCounter={count}/>
         </div>
       </div>
